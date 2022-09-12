c10bceb6470e5f773ad1b0075e4faa8b1d2fc303
noximus <SageM5862@gmail.com>
2022-09-11 23:15:05 -0700

    finish post page

--- connecting_pride/src/pages/postPage/postPage.jsx
+++ connecting_pride/src/pages/postPage/postPage.jsx
@@ -1,10 +1,13 @@
 import React from "react"
+import SideBar from "../../components/sidebar/sidebar"
+import UniquePost from "../../components/uniquePost/uniquePost"
 import "./postPage.css"
 export default function PostPage() {
     return (
         <>
             <div className="postPage">
-                post page
+                <SideBar />
+                <UniquePost/>
             </div>
         </>
     )
