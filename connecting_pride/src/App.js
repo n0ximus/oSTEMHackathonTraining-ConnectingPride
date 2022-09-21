ad83977f27cd365baea3edc9d788a94c7b0b6d41
noximus <SageM5862@gmail.com>
2022-09-21 12:51:16 -0700

    complete writing post page design

--- connecting_pride/src/App.js
+++ connecting_pride/src/App.js
@@ -1,12 +1,13 @@
 import NavBar from "./components/navbar/navbar"
 import Home from "./pages/home/home"
 import PostPage from "./pages/postPage/postPage"
+import WritePost from "./pages/writePost/writePost"
 
 function App() {
     return (
         <>
             <NavBar />
-            <PostPage />
+            <WritePost />
         </>
     );
 }
