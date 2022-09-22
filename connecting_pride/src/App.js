3ea35b1eb430c4432c3959fe56eefd6a0984b2af
noximus <SageM5862@gmail.com>
2022-09-22 12:27:40 -0700

    add links everywhere

--- connecting_pride/src/App.js
+++ connecting_pride/src/App.js
@@ -9,7 +9,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 
 function App() {
-    const currentUser = true;
+    const currentUser = false;
     return (
         <Router>
             <NavBar />
--- connecting_pride/src/pages/login/Login.jsx
+++ connecting_pride/src/pages/login/Login.jsx
@@ -1,5 +1,5 @@
 import "./login.css";
-
+import { Link } from "react-router-dom";
 export default function Login() {
     return (
         <div className="login">
@@ -11,7 +11,10 @@ export default function Login() {
                 <input className="loginInput" type="password" placeholder="password" />
                 <button className="loginButton">Login</button>
             </form>
-            <button className="loginCreateAccountButton">Create an Account</button>
+            <button className="loginCreateAccountButton">
+                <Link className="link" to="/createAccount">
+                    Create an Account
+                </Link></button>
         </div>
     );
 }
