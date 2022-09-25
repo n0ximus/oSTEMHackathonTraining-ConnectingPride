bbbea064564797e2c958ffdd1351f3ae125ca64b
noximus <SageM5862@gmail.com>
2022-09-25 10:18:00 -0700

    complete unique post pageg

--- connecting_pride/frontend/src/components/uniquePost/uniquePost.jsx
+++ connecting_pride/frontend/src/components/uniquePost/uniquePost.jsx
@@ -1,6 +1,63 @@
 import React from "react"
 import "./uniquePost.css"
+import axios from "axios";
+import { useContext, useEffect, useState } from "react";
+import { useLocation } from "react-router";
+import { Link } from "react-router-dom";
+import { Context } from "../../context/Context";
+
 export default function UniquePost() {
+    const PF = "http://localhost:3001/images/";
+    const location = useLocation();
+    const path = location.pathname.split("/")[2]; /* gets the post id */
+    const [post, setPost] = useState({});
+    const { user } = useContext(Context);
+    const [title, setTitle] = useState("");
+    const [description, setDescription] = useState("");
+    const [updateMode, setUpdateMode] = useState(false);
+
+
+    /*
+     * gets the current specified post to display its data to the screen
+     * keeps the post updated post on the screen
+     * */
+    useEffect(() => {
+        const getPost = async () => {
+            const res = await axios.get("/posts/" + path);
+            setPost(res.data);
+            setTitle(res.data.title);
+            setDescription(res.data.description);
+        };
+        getPost();
+    }, [path]);
+
+
+    /*
+     * deletes a specified post
+     * */
+    const handleDelete = async () => {
+        try {
+            await axios.delete(`/posts/${post._id}`, {
+                data: { username: user.username },
+            });
+            window.location.replace("/"); /* go back to home page */
+        } catch (err) { }
+    };
+
+    /* 
+     * handles updating a post 
+     * */
+    const handleUpdate = async () => {
+        try {
+            await axios.put(`/posts/${post._id}`, {
+                username: user.username,
+                title,
+                description,
+            });
+            setUpdateMode(false)
+        } catch (err) { }
+    };
+
     return (
         <>
             <div className="uniquePost">
@@ -8,50 +65,52 @@ export default function UniquePost() {
 
                 <div className="uniquePostWrapper">
                     <h1 className="uniquePostTitle">
-                        Post Title
-
-                        <div className="editPost">
-                            <i className="postIcon fa-solid fa-pen-to-square"></i>
-                            <i className="postIcon fa-solid fa-trash"></i>
-                        </div>
+                        {title}
+                        {post.username === user?.username && (
+                            <div className="editPost">
+                                <i
+                                    className="postIcon far fa-edit"
+                                    onClick={() => setUpdateMode(true)}
+                                ></i>
+                                <i
+                                    className="postIcon far fa-trash-alt"
+                                    onClick={handleDelete}
+                                ></i>
+                            </div>
+                        )}
                     </h1>
 
-                    <img className="uniquePostImage"
-                        src="/images/default_post_image.png"
-                        alt=""
-                    />
+                    {post.photo && (
+                        <img src={PF + post.photo} alt="" className="uniquePostImg" />
+                    )}
 
 
                     <div className="uniquePostInfo">
-                        <span className="uniquePostAuthor"> Author: <b> Sage Meadows </b></span>
-                        <span className="uniquePostDate"> <i> September 12, 2022 </i> </span>
+                        <span className="uniquePostAuthor">            Author:
+                            <Link to={`/?user=${post.username}`} className="link">
+                                <b> {post.username}</b>
+                            </Link>
+                        </span>
+                        <span className="uniquePostDate">             {new Date(post.createdAt).toDateString()}
+                        </span>
 
                     </div>
-                    <p className="uniquePostDescription">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Amet est placerat in egestas erat imperdiet sed euismod. Arcu bibendum at varius vel pharetra. Sagittis vitae et leo duis. Non diam phasellus vestibulum lorem sed risus ultricies. Quis ipsum suspendisse ultrices gravida. Vel elit scelerisque mauris pellentesque pulvinar. Nunc vel risus commodo viverra maecenas accumsan lacus vel. Pellentesque massa placerat duis ultricies lacus sed turpis tincidunt. Habitant morbi tristique senectus et netus et malesuada fames ac. Ut ornare lectus sit amet est placerat. Cras adipiscing enim eu turpis egestas pretium aenean pharetra.
 
-                        Sagittis eu volutpat odio facilisis mauris sit amet massa. Sit amet porttitor eget dolor morbi. Vel eros donec ac odio tempor orci dapibus ultrices in. Est velit egestas dui id ornare. Nulla posuere sollicitudin aliquam ultrices sagittis orci a. Nibh sed pulvinar proin gravida. Maecenas accumsan lacus vel facilisis volutpat est velit egestas dui. Velit euismod in pellentesque massa placerat duis ultricies lacus sed. Tellus pellentesque eu tincidunt tortor aliquam nulla facilisi cras. Lorem mollis aliquam ut porttitor. Vel pretium lectus quam id leo. In nisl nisi scelerisque eu ultrices vitae auctor eu augue. Tellus elementum sagittis vitae et leo duis ut diam quam. Amet luctus venenatis lectus magna fringilla. Amet cursus sit amet dictum sit amet justo donec. Vitae aliquet nec ullamcorper sit. Purus non enim praesent elementum facilisis leo vel. Facilisis mauris sit amet massa vitae tortor condimentum lacinia quis.
+                    {updateMode ? (
+                    <textarea
+                            className="uniquePostDescriptionInput"
+                        value={description  }
+                        onChange={(e) => setDescription(e.target.value)}
+                    />
+                    ) : (
+                    <p className="uniquePostDescription">{description}</p>
+                    )}
 
-                        Augue eget arcu dictum varius duis at. Malesuada fames ac turpis egestas integer eget aliquet nibh praesent. At imperdiet dui accumsan sit amet nulla facilisi morbi tempus. In arcu cursus euismod quis viverra nibh cras pulvinar. Sagittis orci a scelerisque purus. Sit amet nisl purus in mollis nunc sed. Nisi vitae suscipit tellus mauris. Neque convallis a cras semper auctor neque. Proin sed libero enim sed. Rhoncus aenean vel elit scelerisque mauris pellentesque pulvinar. Enim sit amet venenatis urna cursus eget. Nunc sed augue lacus viverra vitae congue eu. Adipiscing elit pellentesque habitant morbi tristique senectus et netus et. Mi eget mauris pharetra et ultrices neque ornare. A iaculis at erat pellentesque adipiscing commodo. Senectus et netus et malesuada fames ac turpis.
-
-                        Velit aliquet sagittis id consectetur purus ut faucibus pulvinar elementum. Sed risus ultricies tristique nulla aliquet enim tortor. Ut aliquam purus sit amet luctus. Nisi vitae suscipit tellus mauris a diam maecenas sed enim. Malesuada bibendum arcu vitae elementum curabitur vitae nunc sed. Faucibus et molestie ac feugiat sed lectus vestibulum. Scelerisque viverra mauris in aliquam sem fringilla. Orci phasellus egestas tellus rutrum tellus pellentesque. Amet consectetur adipiscing elit pellentesque habitant. Arcu non sodales neque sodales ut. Tincidunt lobortis feugiat vivamus at. Nascetur ridiculus mus mauris vitae ultricies leo integer malesuada.
-
-                        Non enim praesent elementum facilisis leo vel fringilla est. Ultrices eros in cursus turpis massa tincidunt dui ut ornare. Odio pellentesque diam volutpat commodo sed egestas egestas. In tellus integer feugiat scelerisque varius. Blandit aliquam etiam erat velit scelerisque in. Magna fringilla urna porttitor rhoncus dolor purus non enim. Dignissim diam quis enim lobortis scelerisque fermentum. Quam nulla porttitor massa id. Condimentum vitae sapien pellentesque habitant morbi. Nibh mauris cursus mattis molestie a iaculis at erat pellentesque. Commodo viverra maecenas accumsan lacus. Mauris pharetra et ultrices neque ornare aenean. Facilisi etiam dignissim diam quis enim lobortis scelerisque. Mattis enim ut tellus elementum sagittis vitae et. Sit amet mauris commodo quis imperdiet massa. Eu nisl nunc mi ipsum faucibus.
-
-                        Vivamus at augue eget arcu. Vel pharetra vel turpis nunc eget lorem dolor. Scelerisque purus semper eget duis at tellus at. Ut sem viverra aliquet eget sit amet tellus cras adipiscing. Vitae ultricies leo integer malesuada nunc vel risus. Turpis cursus in hac habitasse platea dictumst quisque. Mi bibendum neque egestas congue quisque egestas diam in arcu. Rutrum quisque non tellus orci ac auctor. Malesuada pellentesque elit eget gravida cum. Pretium lectus quam id leo in. Tellus in metus vulputate eu scelerisque felis imperdiet proin. Cursus in hac habitasse platea dictumst quisque.
-
-                        Feugiat scelerisque varius morbi enim nunc. Nullam ac tortor vitae purus faucibus ornare suspendisse. Sed vulputate odio ut enim. Semper auctor neque vitae tempus quam pellentesque nec. Eget dolor morbi non arcu risus quis varius. Vitae turpis massa sed elementum tempus egestas sed. Tempus iaculis urna id volutpat lacus. Id neque aliquam vestibulum morbi blandit. Magna fringilla urna porttitor rhoncus dolor purus non enim praesent. Consequat ac felis donec et odio.
-
-                        Iaculis eu non diam phasellus vestibulum. Etiam erat velit scelerisque in dictum non consectetur a. Arcu felis bibendum ut tristique et egestas quis ipsum suspendisse. Fermentum leo vel orci porta non pulvinar neque. Et malesuada fames ac turpis. Vitae et leo duis ut diam quam nulla porttitor. Sapien faucibus et molestie ac feugiat sed lectus. Enim neque volutpat ac tincidunt. Tortor aliquam nulla facilisi cras. Iaculis eu non diam phasellus. Gravida rutrum quisque non tellus orci ac auctor augue. Vestibulum mattis ullamcorper velit sed. Volutpat ac tincidunt vitae semper quis lectus nulla. A scelerisque purus semper eget duis. Etiam erat velit scelerisque in dictum non consectetur a erat. Curabitur vitae nunc sed velit dignissim sodales ut eu sem. Quis hendrerit dolor magna eget est lorem ipsum dolor sit. Eu feugiat pretium nibh ipsum consequat nisl. Vitae ultricies leo integer malesuada nunc. Sed felis eget velit aliquet sagittis id consectetur purus.
-
-                        Urna nunc id cursus metus aliquam. Ac tincidunt vitae semper quis lectus. Sed nisi lacus sed viverra tellus in. Vel orci porta non pulvinar neque laoreet. Elementum nisi quis eleifend quam adipiscing vitae proin sagittis. Elit scelerisque mauris pellentesque pulvinar pellentesque habitant morbi. Et malesuada fames ac turpis egestas integer eget aliquet nibh. Amet commodo nulla facilisi nullam vehicula ipsum a arcu. Ornare suspendisse sed nisi lacus sed. Cursus in hac habitasse platea dictumst. Et netus et malesuada fames ac turpis egestas sed tempus. Augue neque gravida in fermentum et sollicitudin ac orci phasellus. Eu scelerisque felis imperdiet proin fermentum. Augue neque gravida in fermentum et. Tristique senectus et netus et malesuada fames ac. Placerat duis ultricies lacus sed turpis tincidunt id.
-
-                        Justo laoreet sit amet cursus. Amet consectetur adipiscing elit duis tristique. Justo eget magna fermentum iaculis eu non diam. Velit scelerisque in dictum non. Eu sem integer vitae justo eget magna fermentum iaculis eu. Nunc scelerisque viverra mauris in. Faucibus purus in massa tempor nec feugiat. Bibendum est ultricies integer quis auctor. Facilisis gravida neque convallis a cras semper auctor neque. A cras semper auctor neque vitae tempus. Praesent tristique magna sit amet purus gravida quis.
-
-                        Nisl rhoncus mattis rhoncus urna neque viverra justo. Ut sem viverra aliquet eget sit. Amet porttitor eget dolor morbi non arcu risus. Varius vel pharetra vel turpis. Diam vel quam elementum pulvinar etiam non. Venenatis a condimentum vitae sapien pellentesque habitant morbi. Ultrices vitae auctor eu augue ut lectus. Ut venenatis tellus in metus vulputate eu scelerisque. Et tortor consequat id porta nibh venenatis cras sed. Vestibulum morbi blandit cursus risus at ultrices mi tempus imperdiet. Amet est placerat in egestas erat imperdiet sed. Facilisi etiam dignissim diam quis enim lobortis scelerisque fermentum. Viverra ipsum nunc aliquet bibendum enim facilisis. Fames ac turpis egestas integer eget aliquet nibh. Ligula ullamcorper malesuada proin libero nunc consequat interdum varius. Nulla aliquet enim tortor at.
-
-
-                    </p>
+                    {updateMode && (
+                        <button className="uniquePostButton" onClick={handleUpdate}>
+                            update
+                        </button>
+                    )}
                 </div>
             </div>
         </>
