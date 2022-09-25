ab055997160db54302c8e00460cb2fdad87b1edb
noximus <SageM5862@gmail.com>
2022-09-24 18:50:19 -0700

    add needed files and connect mongoose

--- connecting_pride/api/index.js
+++ connecting_pride/api/index.js
@@ -1,6 +1,23 @@
 const express = require("express");
 const app = express();
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+dotenv.config();
 
+mongoose.connect(process.env.MONGO_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: true
+    })
+    .then(console.log("MongoDB Connected"))
+    .catch((err) => console.log(err));
+
+
+app.use("/", (req, res) =>
+{
+    console.log("this is the home")
+})
 app.listen("5000", () => {
     console.log("Backend is running.");
 });
